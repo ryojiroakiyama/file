5ab9ccb0fe587928c646a1f0dd8502c80140d3bc
rakiyama <ryojiro25@gmail.com>
2022-02-12 21:58:36 +0900

    add comment

--- file.go
+++ file.go
@@ -1,3 +1,4 @@
+// Package file provides basic functions for file operations.
 package file
 
 import (
@@ -7,33 +8,75 @@ import (
 	"os"
 )
 
+//ToFile creates a fileName file and writes contents to the file.
+//If successful, ToFile returns nil error.
+//Else returns any error encountered.
+func ToFile(fileName string, contents []byte) (err error) {
+	dstFile, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("ToFile: %v", err)
+	}
+	defer func() {
+		if cerr := dstFile.Close(); cerr != nil {
+			err = fmt.Errorf("ToFile: %v", cerr)
+		}
+		if err != nil && dstFile != nil {
+			os.Remove(dstFile.Name())
+		}
+	}()
+	_, werr := dstFile.Write(contents)
+	if werr != nil {
+		return fmt.Errorf("ToFile: %v", werr)
+	}
+	return
+}
+
+//ToTmpFile creates a tmpprary file and write contents to the file.
+//ToFile returns string which is name of the file created.
+// any error encountered.
 func ToTmpFile(src io.Reader) (fileName string, err error) {
 	tmpfile, err := os.CreateTemp("", "")
 	if err != nil {
-		return "", fmt.Errorf("failt to create tmpfile: %v", err)
+		return "", fmt.Errorf("ToTmpFile: %v", err)
 	}
 	fileName = tmpfile.Name()
-	fmt.Println("create:", fileName)
+	//fmt.Println("create:", fileName)
 	defer func() {
 		if cerr := tmpfile.Close(); cerr != nil {
-			err = fmt.Errorf("fail to close: %v", cerr)
+			err = fmt.Errorf("ToTmpFile: %v", cerr)
+		}
+		if err != nil && fileName != "" {
+			os.Remove(fileName)
 		}
 	}()
 	_, err = io.Copy(tmpfile, src)
 	if err != nil {
-		return "", fmt.Errorf("fail to copy: %v", err)
+		return "", fmt.Errorf("ToTmpFile: %v", err)
 	}
 	return
+}
+
+func FileBytes(fileName string) ([]byte, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("FileBytes: %v", err)
+	}
+	defer file.Close()
+	srcBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("FileBytes: %v", err)
+	}
+	return srcBytes, nil
 }
 
 func BindFiles(srcNames []string, dstName string) (err error) {
 	dstfile, err := os.Create(dstName)
 	if err != nil {
-		return fmt.Errorf("failt to create: %v", err)
+		return fmt.Errorf("BindFiles: %v", err)
 	}
 	defer func() {
 		if cerr := dstfile.Close(); cerr != nil {
-			err = fmt.Errorf("fail to close: %v", cerr)
+			err = fmt.Errorf("BindFiles: %v", cerr)
 		}
 		if err != nil {
 			os.Remove(dstName)
@@ -42,43 +85,13 @@ func BindFiles(srcNames []string, dstName string) (err error) {
 	for _, srcName := range srcNames {
 		srcfile, err := os.Open(srcName)
 		if err != nil {
-			return fmt.Errorf("failt to open: %v", err)
+			return fmt.Errorf("BindFiles: %v", err)
 		}
 		defer srcfile.Close()
 		_, err = io.Copy(dstfile, srcfile)
 		if err != nil {
-			return fmt.Errorf("fail to copy: %v", err)
+			return fmt.Errorf("BindFiles: %v", err)
 		}
 	}
 	return err
-}
-
-func FileBytes(fileName string) ([]byte, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, fmt.Errorf("fail to open: %v", err)
-	}
-	defer file.Close()
-	srcBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, fmt.Errorf("fail to read: %v", err)
-	}
-	return srcBytes, nil
-}
-
-func ToFile(fileName string, contents []byte) (err error) {
-	dstFile, err := os.Create(fileName)
-	if err != nil {
-		return fmt.Errorf("fail to create: %v", err)
-	}
-	defer func() {
-		if cerr := dstFile.Close(); cerr != nil {
-			err = fmt.Errorf("fail to close: %v", cerr)
-		}
-	}()
-	_, werr := dstFile.Write(contents)
-	if werr != nil {
-		return fmt.Errorf("fail to write: %v", werr)
-	}
-	return
 }
